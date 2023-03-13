4a8b4bf0b3c7b54efd2432e938aaf5ea28ba0ec6
fathyadel0 <stfncrwl0@gmail.com>
2023-03-13 10:16:12 +0200

    add code

--- dist/FindUniq.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function findUniq(arr) {
-    const array = arr.sort();
-    return array[0] == array[1] ? array[array.length - 1] : array[0];
-}
-const uniqueNumber = findUniq([1, 1, 2, 1, 1, 1]);
-console.log(uniqueNumber);
--- dist/SpinWords.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function spinWords(words) {
-    return words
-        .split(" ")
-        .map((word) => word.length >= 5 ? word.split("").reverse().join("") : word)
-        .join(" ");
-}
-console.log(spinWords("This is another test"));
